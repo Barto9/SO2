0b7efe6c40cd3d030a8e00a8f2361c38e5746032
Barto9 <62943350+Barto9@users.noreply.github.com>
2025-03-23 17:29:27 +0100

    Comentary

--- Filozofowie/Filozofowie/Filozofowie.cpp
+++ Filozofowie/Filozofowie/Filozofowie.cpp
@@ -1,14 +1,13 @@
 ﻿#include <iostream>
 #include <thread>
 #include <mutex>
-#include <condition_variable>
 #include <vector>
 #include <chrono>
 #include <random>
 
 const int NUM_PHILOSOPHERS = 5;
-std::mutex forks[NUM_PHILOSOPHERS];
-std::mutex cout_mutex;
+std::mutex forks[NUM_PHILOSOPHERS]; //array of mutex's (is this how you write it?XD) each one is a fork
+std::mutex cout_mutex; //one guy talking at a time so stuff will actually make sense
 
 void think(int id) {
     std::this_thread::sleep_for(std::chrono::milliseconds(100 + rand() % 900));
@@ -17,43 +16,45 @@ void think(int id) {
 void eat(int id) {
     std::this_thread::sleep_for(std::chrono::milliseconds(100 + rand() % 900));
 }
-
+//both think and eat is in a range of 100 - 999 ms
 void philosopher(int id) {
     while (true) {
         think(id);
-        std::cout << "Filozof " << id << " mysli.\n";
-        int left = id;
-        int right = (id + 1) % NUM_PHILOSOPHERS;
+        std::cout << "Philosopher " << id << " thinks.\n";
+        int left = id; //left fork
+        int right = (id + 1) % NUM_PHILOSOPHERS; //right fork
 
-        if (id % 2 == 0) {
+        //HERE IS THE VERY IMPORTANT PART
+        if (id % 2 == 0) { //even dudes pick up the left one first
             forks[left].lock();
             forks[right].lock();
         }
-        else {
+        else { //uneven dudes pick up the right one first
             forks[right].lock();
             forks[left].lock();
-        }
+        } //thanks to that we won't have a situation where they all they the left fork
+          //and consequently that lead to a deadlock
 
         {
-            std::lock_guard<std::mutex> lock(cout_mutex);
-            std::cout << "Filozof " << id << " je.\n";
+            std::lock_guard<std::mutex> lock(cout_mutex); //one guy talks at a time
+            std::cout << "Philosopher " << id << " eats.\n";
         }
 
-        eat(id);
+        eat(id); //we eat, and when we are done
 
-        forks[left].unlock();
+        forks[left].unlock(); //put the forks down
         forks[right].unlock();
     }
 }
 
 int main() {
-    std::vector<std::thread> philosophers;
+    std::vector<std::thread> philosophers; //vector of threads
     for (int i = 0; i < NUM_PHILOSOPHERS; i++) {
-        philosophers.emplace_back(philosopher, i);
+        philosophers.emplace_back(philosopher, i); //each philosopher gets his own thread
     }
 
     for (auto& p : philosophers) {
-        p.join();
+        p.join(); //pohilosophers don't stop eating, so program never endsXD
     }
 
     return 0;
